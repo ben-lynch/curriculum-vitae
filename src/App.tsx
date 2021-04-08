b589876356bdadddf86cf097225c1ac1ddf05426
Ben Lynch <ben@iridiumdesigns.co.uk>
2021-04-08 13:25:06 +0100

    Sort out imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent } from 'react'
+import { FunctionComponent } from 'react'
+
 import { Typography } from './components'
 
 export const App: FunctionComponent = () => (
